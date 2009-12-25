08da349df53a85113219965c70e54f7d57e44b65
Mark Probst <mark.probst@gmail.com>
2009-12-25 08:29:13 +0100

    Beginning to abstract out the REPL - make macroexpand toplevel.

--- jscreme.scm
+++ jscreme.scm
@@ -13,47 +13,37 @@
 			      (cons expr (recur)))))))
 	(recur)))))
 
+(load "repl.scm")
+
 (define (compile-toplevels exprs)
-  (letrec ((macroexpand (lambda (expr macros)
-			  (if (pair? expr)
-			      (if (eq? (car expr) 'quote)
-				  expr
-				  (let ((binding (assq (car expr) macros))
-					(rest (map (lambda (x) (macroexpand x macros)) (cdr expr))))
-				    (if binding
-					(macroexpand (apply (cdr binding) rest) macros)
-					(cons (macroexpand (car expr) macros) rest))))
-			      expr)))
-	   (recur (lambda (so-far exprs env macros)
+  (letrec ((recur (lambda (so-far exprs env)
 		    (cond ((null? exprs)
 			   so-far)
 			  ((and (pair? (car exprs)) (eq? (caar exprs) 'defmacro))
 			   (let ((name (cadar exprs))
 				 (args (caddar exprs))
 				 (value (car (cdddar exprs))))
-			     (recur so-far
-				    (cdr exprs) env
-				    (cons (cons name (eval (list 'lambda args
-								 (macroexpand value macros))
-							   (interaction-environment)))
-					  macros))))
+			     (add-macro name (eval (list 'lambda args
+							 (macroexpand value))
+						   (interaction-environment)))
+			     (recur so-far (cdr exprs) env)))
 			  (else
-			   (let ((expr (macroexpand (car exprs) macros)))
+			   (let ((expr (macroexpand (car exprs))))
 			     (cond ((and (pair? expr) (eq? (car expr) 'js-define))
 				    (let* ((name (cadr expr))
 					   (value (caddr expr))
 					   (var-name (jsify-symbol name))
 					   (new-env (cons (cons name var-name) env)))
 				      (recur (string-append so-far "var " var-name "="
-							    (compile (macroexpand value macros) new-env) ";\n")
-					     (cdr exprs) new-env macros)))
+							    (compile (macroexpand value) new-env) ";\n")
+					     (cdr exprs) new-env)))
 				   ((and (pair? expr) (eq? (car expr) 'load))
 				    (let ((more-exprs (read-file (cadr expr))))
-				      (recur so-far (append more-exprs (cdr exprs)) env macros)))
+				      (recur so-far (append more-exprs (cdr exprs)) env)))
 				   (else
 				    (recur (string-append so-far (compile expr env) ";\n")
-					   (cdr exprs) env macros)))))))))
-    (recur "" exprs '() '())))
+					   (cdr exprs) env)))))))))
+    (recur "" exprs '())))
 
 (call-with-output-file "tests.js"
   (lambda (port)
