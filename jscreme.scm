b709f24d155a1041bfc0384f6edd91e6601c28f0
Mark Probst <mark.probst@gmail.com>
2009-12-25 10:47:06 +0100

    Toplevel compilation abstraced out.

--- jscreme.scm
+++ jscreme.scm
@@ -16,34 +16,7 @@
 (load "repl.scm")
 
 (define (compile-toplevels exprs)
-  (letrec ((recur (lambda (so-far exprs env)
-		    (cond ((null? exprs)
-			   so-far)
-			  ((and (pair? (car exprs)) (eq? (caar exprs) 'defmacro))
-			   (let ((name (cadar exprs))
-				 (args (caddar exprs))
-				 (value (car (cdddar exprs))))
-			     (add-macro name (eval (list 'lambda args
-							 (macroexpand value))
-						   (interaction-environment)))
-			     (recur so-far (cdr exprs) env)))
-			  (else
-			   (let ((expr (macroexpand (car exprs))))
-			     (cond ((and (pair? expr) (eq? (car expr) 'js-define))
-				    (let* ((name (cadr expr))
-					   (value (caddr expr))
-					   (var-name (jsify-symbol name))
-					   (new-env (cons (cons name var-name) env)))
-				      (recur (string-append so-far "var " var-name "="
-							    (compile (macroexpand value) new-env) ";\n")
-					     (cdr exprs) new-env)))
-				   ((and (pair? expr) (eq? (car expr) 'load))
-				    (let ((more-exprs (read-file (cadr expr))))
-				      (recur so-far (append more-exprs (cdr exprs)) env)))
-				   (else
-				    (recur (string-append so-far (compile expr env) ";\n")
-					   (cdr exprs) env)))))))))
-    (recur "" exprs '())))
+  (fold-left string-append "" (map (lambda (x) (toplevel-compile x read-file)) exprs)))
 
 (call-with-output-file "tests.js"
   (lambda (port)
--- repl.scm
+++ repl.scm
@@ -5,8 +5,11 @@
 
 (define *toplevel-macros* '())
 
-(define (add-macro name func)
+(define (add-toplevel-macro name func)
   (set! *toplevel-macros* (cons (cons name func) *toplevel-macros*)))
+
+(define (add-toplevel-binding name var-name)
+  (set! *toplevel-bindings* (cons (cons name var-name) *toplevel-bindings*)))
 
 (define (macroexpand expr)
   (if (pair? expr)
@@ -18,3 +21,26 @@
 		(macroexpand (apply (cdr binding) rest))
 		(cons (macroexpand (car expr)) rest))))
       expr))
+
+(define (toplevel-compile expr read-file)
+  (if (and (pair? expr) (eq? (car expr) 'defmacro))
+      (let ((name (cadr expr))
+	    (args (caddr expr))
+	    (value (car (cdddr expr))))
+	(add-toplevel-macro name (eval (list 'lambda args
+					     (macroexpand value))
+				       (interaction-environment)))
+	"")
+      (let ((expr (macroexpand expr)))
+	(cond ((and (pair? expr) (eq? (car expr) 'js-define))
+	       (let* ((name (cadr expr))
+		      (value (caddr expr))
+		      (var-name (jsify-symbol name)))
+		 (add-toplevel-binding name var-name)
+		 (string-append "var " var-name "="
+				(compile (macroexpand value) *toplevel-bindings*) ";\n")))
+	      ((and (pair? expr) (eq? (car expr) 'load))
+	       (let ((more-exprs (read-file (cadr expr))))
+		 (fold-left string-append "" (map (lambda (x) (toplevel-compile x read-file)) more-exprs))))
+	      (else
+	       (string-append (compile expr *toplevel-bindings*) ";\n"))))))
